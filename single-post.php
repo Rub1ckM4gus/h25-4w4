2a3adc6b11eefdbe53b30fe0228e6be1e6d3aa4c
Alexis David <alexis.david.111@gmail.com>
2025-03-17 13:55:21 -0400

    s9c1, article extra contenant (h2 & div) retiré de single-post.php

--- single-post.php
+++ single-post.php
@@ -2,11 +2,7 @@
 <h1>-------------- single-post.php ---------------</h1>
     <section class="populaire">
         <div class="global">
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <article>
-                <h2><?php the_title(); ?></h2>
-                <div><?php the_content(); ?></div>
-            </article>     
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>   
             <?php
                 if (has_post_thumbnail()){
                 the_post_thumbnail('large');}
