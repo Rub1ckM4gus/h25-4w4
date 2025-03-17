bc648415ebc83fc13fe29560253a9fa50da5930c
Alexis David <alexis.david.111@gmail.com>
2025-03-17 14:52:07 -0400

    s9c1: ajustement pour affichage de Température Maximale et texte pour lien vers single-post.php, chamgement dans carte.php

--- gabarit/carte.php
+++ gabarit/carte.php
@@ -13,8 +13,8 @@
     ?>
     <h4 class="carte__titre"><?php the_title(); ?></h4>
     <p class="carte__description"><?php echo wp_trim_words(the_content(),10, "..." ); ?></p>
-    <a class="carte__bouton carte__bouton--actif" href="<?php the_permalink()?>">suite ...</a>
+    <a class="carte__bouton carte__bouton--actif" href="<?php the_permalink()?>">information supplémentaire...</a>
     <?php the_category(); ?>
-    <p> Température Maximum: <?php the_field('Température Maximum');?> C </p>
+    <p> Température Maximale: <?php the_field('temperature_maximale');?> C </p>
   </div>
 </article>
