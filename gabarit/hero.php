3e1e88881c4d39f397234937da3ceaac786f9f85
Rub1ckM4gus <118757722+Rub1ckM4gus@users.noreply.github.com>
2025-04-14 13:54:30 -0400

    s12c1, ajout bouton radio dans hero.php

--- gabarit/hero.php
+++ gabarit/hero.php
@@ -4,12 +4,18 @@
     $hero_background[$k] = get_theme_mod('hero_background' . $k, '');
     }
 ?>
-    
 
 <section class="hero" style="background-image: url(<?php echo $hero_background ?>)">
     <div class="hero__carrousel" style="background-image: url(<?php echo $hero_background[0] ?>)"></div>
     <div class="hero__carrousel" style="background-image: url(<?php echo $hero_background[1] ?>)"></div>
     <div class="hero__carrousel" style="background-image: url(<?php echo $hero_background[2] ?>)"></div>
+
+    <div class="hero__radio">
+        <input type="radio" name="carroussel" id="">
+        <input type="radio" name="carroussel" id="">
+        <input type="radio" name="carroussel" id="">
+    </div>
+
     <div class="hero__contenu global">
         <h1 class="hero__titre"><?php bloginfo('name');?></h1>
         <p class="hero__description">
