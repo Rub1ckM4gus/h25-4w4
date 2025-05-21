dd5b9f3cc6ef3cdce9f2680359d51ff51b36281e
Rub1ckM4gus <118757722+Rub1ckM4gus@users.noreply.github.com>
2025-05-20 20:02:39 -0400

    intégration de la vague dans footer.php ainsi que update dans generateur.php, ; manquant dans l'appel du footer

--- gabarit/hero.php
+++ gabarit/hero.php
@@ -28,22 +28,19 @@
             <div class="hero__animation <?php echo $k === 0 ? 'hero__animation--active' : ''; ?>">
                 <h1 class="hero__titre"><?php bloginfo('name');?></h1>
                 <p class="hero__description"><?php bloginfo('description');?></p>
+                <p class="hero__courriel"><?php bloginfo('admin_email');?></p>
+                <p class="hero__adresse">5800 Sherbrooke-est - Montréal (Québec) H1X 2A2</p>
+                <p class="hero__auteur">Auteur: <?php echo $hero_auteur?> </p>
+                <div class="hero__icone">
+                    <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
+                    <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
+                    <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="20" height="20">
+                    <img src="https://s2.svgbox.net/social.svg?ic=snapchat&color=000000" width="20" height="20">
+                    <img src="https://s2.svgbox.net/social.svg?ic=github&color=000000" width="20" height="20">
+                </div>
             </div>
         <?php } ?>
 
-        <p class="hero__courriel">
-        <?php bloginfo('admin_email');?>
-        </p>
-        <p class="hero__adresse">
-            5800 Sherbrooke-est - Montréal (Québec) H1X 2A2
-        </p>
-        <p class="hero_auteur">Auteur: <?php echo $hero_auteur?> </p>
-        <div class="hero__icone">
-            <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
-            <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
-            <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="20" height="20">
-            <img src="https://s2.svgbox.net/social.svg?ic=snapchat&color=000000" width="20" height="20">
-            <img src="https://s2.svgbox.net/social.svg?ic=github&color=000000" width="20" height="20">
-        </div>
+        
     </div>
 </section>
